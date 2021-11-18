08884fb267a8e132675e12c1996d85898524a71b
deadislove <taweilin7689@gmail.com>
2021-11-18 11:26:04 +0800

    Add the memento pattern 11/18/21 11:25 AM

--- README.md
+++ README.md
@@ -84,7 +84,9 @@ This repository provide the enterprise architecture level sample projects. The g
 \
 [Behavioral Patterns - Iterator](https://davidskyspace.com/behavioral-patterns-iterator/)
 \
-[Behavioral Patterns - Mediator ](https://davidskyspace.com/behavioral-patterns-mediator/)
+[Behavioral Patterns - Mediator](https://davidskyspace.com/behavioral-patterns-mediator/)
+\
+[Behavioral Patterns - Memento](https://davidskyspace.com/behavioral-patterns-memento/)
 
 ### OTHER PART:
 [CRUD – Transaction Behavior in Entity Framework Core](https://davidskyspace.com/crud-transaction-behavior-in-entity-framework-core/)
@@ -102,6 +104,7 @@ This repository provide the enterprise architecture level sample projects. The g
    - Enterprise_Dot_Net_Core_WebApp web project
    - Enterprise_Dot_Net_Core_WebApp.Core core project
    - Enterprise_Dot_Net_Core_WebApp.Infra infra project.
+   - Enterprise_Dot_Net_Core_WebApp.SharedKernel shared kernel project
 
 ## Microsoft Doc reference
 - [Common web application architectures](https://docs.microsoft.com/en-us/dotnet/architecture/modern-web-apps-azure/common-web-application-architectures)
---
+++ src/ASP.NET Core/Enterprise_Dot_Net_Core_WebApp.Core/DesignPatterns/Memento/ConcreteMemento.cs
@@ -0,0 +1,58 @@
+﻿using Enterprise_Dot_Net_Core_WebApp.Core.Interface.DesignPatterns.Memento;
+using System;
+
+namespace Enterprise_Dot_Net_Core_WebApp.Core.DesignPatterns.Memento
+{
+    public class ConcreteMemento<T> : IMemento<T>, IDisposable where T : class
+    {
+        private T _State;
+        private DateTime _Date;
+
+        public ConcreteMemento(T State)
+        {
+            _State = State;
+            _Date = DateTime.Now;
+        }
+
+        public DateTime GetDate() => _Date;
+
+        public T ObjectStatus() => _State;
+        
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        ~ConcreteMemento()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(false);
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
